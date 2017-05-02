8f52cac4d40743c3a4afc2f8a262a1aa263c4038
Hormash <hormash.ro@gmail.com>
2017-05-01 22:09:13 -0300

    32buttonsDisk
    up to 32 buttons and without HID

---
+++ ard32disck/Joystick.cpp
@@ -0,0 +1,91 @@
+/*
+  Joystick.cpp
+
+  Copyright (c) 2015, Matthew Heironimus
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "Joystick.h"
+
+#if defined(_USING_HID)
+
+#define JOYSTICK_REPORT_ID 0x03
+#define JOYSTICK_STATE_SIZE 6
+
+static const uint8_t _hidReportDescriptor[] PROGMEM = {
+  
+	// Joystick
+	0x05, 0x01,			      // USAGE_PAGE (Generic Desktop)
+	0x09, 0x04,			      // USAGE (Joystick)
+	0xa1, 0x01,			      // COLLECTION (Application)
+	0x85, JOYSTICK_REPORT_ID, //   REPORT_ID (3)
+
+	// 32 Buttons
+	0x05, 0x09,			      //   USAGE_PAGE (Button)
+	0x19, 0x01,			      //   USAGE_MINIMUM (Button 1)
+	0x29, 0x20,			      //   USAGE_MAXIMUM (Button 32)
+	0x15, 0x00,			      //   LOGICAL_MINIMUM (0)
+	0x25, 0x01,			      //   LOGICAL_MAXIMUM (1)
+	0x75, 0x01,			      //   REPORT_SIZE (1)
+	0x95, 0x20,			      //   REPORT_COUNT (32)
+	0x55, 0x00,			      //   UNIT_EXPONENT (0)
+	0x65, 0x00,			      //   UNIT (None)
+	0x81, 0x02,			      //   INPUT (Data,Var,Abs)
+
+	// X, Y, and Z Axis
+  0x05, 0x01,                    /*     USAGE_PAGE (Generic Desktop) */ 
+  0x09, 0x30,                    /*     USAGE (X) */ 
+  0x09, 0x31,                    /*     USAGE (Y) */ 
+  0x15, 0x81,                    /*     LOGICAL_MINIMUM (0) */ 
+  0x25, 0x7f,                    /*     LOGICAL_MAXIMUM (255) */ 
+  0x95, 0x02,                    /*     REPORT_COUNT (3) */ 
+  0x75, 0x08,                    /*     REPORT_SIZE (8) */ 
+  0x81, 0x02,                    /*     INPUT (Data,Var,Abs) */ 
+                              
+	0xc0				      // END_COLLECTION
+};
+
+Joystick_::Joystick_()
+{
+	// Setup HID report structure
+	static HIDSubDescriptor node(_hidReportDescriptor, sizeof(_hidReportDescriptor));
+	HID().AppendDescriptor(&node);
+}
+
+void Joystick_::sendState()
+{
+	uint8_t data[JOYSTICK_STATE_SIZE];
+	uint32_t buttonTmp = buttons;
+
+	// Split 32 bit button-state into 4 bytes
+	data[0] = buttonTmp & 0xFF;		
+	buttonTmp >>= 8;
+	data[1] = buttonTmp & 0xFF;
+	buttonTmp >>= 8;
+	data[2] = buttonTmp & 0xFF;
+	buttonTmp >>= 8;
+	data[3] = buttonTmp & 0xFF;
+
+	data[4] = xAxis;
+	data[5] = yAxis;
+
+	// HID().SendReport(Report number, array of values in same order as HID descriptor, length)
+	HID().SendReport(JOYSTICK_REPORT_ID, data, JOYSTICK_STATE_SIZE);
+}
+
+Joystick_ Joystick;
+
+#endif
---
+++ ard32disck/ard32disck.ino
@@ -0,0 +1,89 @@
+// Program used to test the USB Joystick object on the 
+// Arduino Leonardo or Arduino Micro.
+//
+// Matthew Heironimus
+// 2015-03-28
+// Updated on 2015-11-18 to use the new Joystick library written for version 1.6.6.
+//------------------------------------------------------------
+
+#include "Joystick.h"
+
+#define REPORT_DELAY 2000
+
+#define ENCODER_Z_A 50
+#define ENCODER_Z_B 51
+#define ENCODER_Z_B_REGISTER 12
+#define ENCODER_Z_MILLIS_TOLERANCE 100 // Amount of miliseconds to wait and change state of turntable buttons
+
+#define ENC_L_A 2
+#define ENC_L_B 3
+#define ENC_L_B_ADDR 0
+#define ENCODER_SENSITIVITY (double) 0.4686
+#define ENCODER_PORT PIND
+
+volatile int32_t encX = 0, encY = 0, encZ = 0, encZlast = 0, encZmillis = 0; // Storage for encoder states
+
+int tmp;
+uint8_t buttonCount = 9;
+uint8_t buttonPins[] = {13,23,22,21,20,19,18,12,11};
+int32_t encL=0;
+
+void doEncL(){
+  if((ENCODER_PORT >> ENC_L_B_ADDR)&1){
+    encL++;
+  } else {
+    encL--;
+  }
+}
+
+void setup() {
+  delay(1000);
+
+  for(int i=0;i<buttonCount;i++){
+    pinMode(buttonPins[i],INPUT_PULLUP);
+  }
+
+  pinMode(ENC_L_A,INPUT_PULLUP);
+  pinMode(ENC_L_B,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENC_L_A), doEncL, RISING);
+}
+
+void loop() {
+ 
+  for(int i=0;i<buttonCount;i++){
+    if(digitalRead(buttonPins[i])!=HIGH){
+      Joystick.buttons |= (uint16_t)1 << i;
+    } else {
+      Joystick.buttons &= ~((uint16_t)1 << i);
+    }
+  }
+
+  Joystick.xAxis = (uint8_t)((int32_t)(encL / ENCODER_SENSITIVITY) % 256);
+  
+  if( (int32_t)(encL / ENCODER_SENSITIVITY) - encZlast > 5) {
+    if(millis() - encZmillis > ENCODER_Z_MILLIS_TOLERANCE || bitRead(Joystick.buttons,10)) {
+      Joystick.buttons |= (uint16_t)1 << 10;
+      Joystick.buttons &= ~((uint16_t)1 << 11);
+      encZlast = (encL / ENCODER_SENSITIVITY);
+      encZmillis = millis();
+    }
+  } else if ( (int32_t)(encL / ENCODER_SENSITIVITY) - encZlast < -5){
+    if(millis() - encZmillis > ENCODER_Z_MILLIS_TOLERANCE || bitRead(Joystick.buttons,11)) {
+      Joystick.buttons |= (uint16_t)1 << 11;
+      Joystick.buttons &= ~((uint16_t)1 << 10);
+      encZlast = (encL / ENCODER_SENSITIVITY);
+      encZmillis = millis();
+    }
+  } else {
+    if(millis() - encZmillis > ENCODER_Z_MILLIS_TOLERANCE) {
+      Joystick.buttons &= ~((uint16_t)1 << 10);
+      Joystick.buttons &= ~((uint16_t)1 << 11);
+    }
+  }
+  Joystick.sendState();
+  delayMicroseconds(REPORT_DELAY);
+}
+
+
+
+
